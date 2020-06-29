ccc5545088af7d915f7cb183e03163a676a6b835
krisvanlog <66163203+krisvanlog@users.noreply.github.com>
2020-06-29 08:44:02 -0700

    Modified For MacbookAir
    Modified for MacBookAir

--- _PushToMT4Prod.bat
+++ _PushToMT4Prod.bat
@@ -4,11 +4,11 @@ rem Use when you need to publish all files to All Terminals
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\WatchDog"
-set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Experts\00_WatchDog"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\00_WatchDog"
-set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Experts\00_WatchDog"
-set DEST_DIR4="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Experts\00_WatchDog"
+set SOURCE_DIR="C:\Users\Kris\Documents\GitHub\LazyTrade\WatchDog"
+set DEST_DIR1="C:\Program Files (x86)\MetaTrader - AxiTrader - Term1\MQL4\Experts\01_WatchDog"
+set DEST_DIR2="C:\Program Files (x86)\MetaTrader - AxiTrader - Term2\MQL4\Experts\02_WatchDog"
+set DEST_DIR3="C:\Program Files (x86)\MetaTrader - AxiTrader - Term3\MQL4\Experts\03_WatchDog"
+set DEST_DIR4="C:\Program Files (x86)\MetaTrader - AxiTrader - Term4\MQL4\Experts\04_WatchDog"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.mq4
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mq4
